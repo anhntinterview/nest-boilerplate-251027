80b88dc08fae0edbf0226180cceb032a15789a1a
Robert Nguyen <tuananhnguyen@Tuans-Mac-mini.local>
2025-10-27 18:04:33 +0700

    ci: Validate + Test + Build + Deploy Nest

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Bootstrap');
+
+  const nodeEnv = process.env.NODE_ENV || 'local';
+  const port = process.env.NEST_PORT || 3001;
+
+  // Create the Nest application
+  const app = await NestFactory.create(AppModule, {
+    cors: nodeEnv === 'local', // enable CORS only for local development
+  });
+
+  if (nodeEnv === 'production') {
+    app.setGlobalPrefix('api');
+  }
+
+  await app.listen(port);
+
+  logger.log(
+    `🚀 Application is running in [${nodeEnv}] mode on: http://localhost:${port}`,
+  );
 }
 bootstrap();
